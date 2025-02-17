988ea3b747b1e1a216c61731d5a7bcd9e9116731
anniegbryant <anniegbryant@gmail.com>
2025-02-17 17:36:14 +1100

    repo reorganization for results

--- DataProcessing/GiveMeFile.m
+++
@@ -1,9 +0,0 @@
-function theFileName = GiveMeFile(whatFile)
-
-switch whatFile
-case 'OCDA_toolbox'
-    theFileName = '/home/patrick/Documents/OverlappingCommunityDetection';
-end
-
-
-end
--- connectome_preprocessing/make_groupAdj.m
+++ connectome_preprocessing/make_groupAdj.m
@@ -1,28 +1,43 @@
 % This script makes group connectomes from HCP data
 
-parcellation = 'HCPMMP1'; %'HCPMMP1' , 'custom200', custom500, aparcaseg
-tract = 'iFOD2'; % FACT vs iFOD2
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Define data path and imaging parameters
+diffusion_data_path='../data/HCP_Connectome/';
+
+% Parcellation: options are 'HCPMMP1' , 'custom200', 'custom500',
+% 'aparc+aseg'
+% We use 'HCPMMP1' to work with the Glasser (2016) 360-region atlas
+parcellation = 'HCPMMP1';
+
+% Tractography type: FACT or iFOD2; we use iFOD2 here
+tract = 'iFOD2';
+
+% SIFT type: SIFT or SIFT2; we use SIFT2 here
 sift = 'SIFT2'; 
-weight = 'standard'; % specify weight to be used:
-% FA
-% standard
-% density
-% MD
-groupMatrixType = 'variance'; % specify type of group matrix
+
+% Weighting to be used: FA, standard, density, or MD; we use standard
+weight = 'standard';
+
+% How to generate group adjacency matrix:
 % 'variance' - Robert's method
 % 'consistency' - VDH method
 % 'lengthCV' - Misic's method
-threshold = 0.90; % consistency threshold 
-subjRem = 299; % subject in a cell 299 consistently has weird connectomes, so remove it
+% groupMatrixType = 'variance';
+groupMatrixType = 'variance';
+threshold = 0.9;
+dens = 0.15;
 
-Conn = load(sprintf('%sANDfslatlas20_acpc_%s_%s_%s_structnets.mat', parcellation, tract, sift, weight));
-Length = load(sprintf('%sANDfslatlas20_acpc_%s_%s_length_structnets.mat', parcellation, tract, sift));
+% Load data
+SC_data = load(sprintf('%s/%sANDfslatlas20_acpc_%s_%s_%s_structnets.mat', diffusion_data_path, parcellation, tract, sift, weight));
+length_data = load(sprintf('%s/%sANDfslatlas20_acpc_%s_%s_length_structnets.mat', diffusion_data_path, parcellation, tract, sift));
 
-coordinates = Conn.COG;
-connectomes = Conn.ADJS;
-distances = Length.ADJS;
+% Extract coordinates, connectomes, and distances
+coordinates = SC_data.COG;
+connectomes = SC_data.ADJS;
+distances = length_data.ADJS;
 
 % remove subjects with weird connectomes
+subjRem = 299; % subject in a cell 299 consistently has weird connectomes, so remove it
 connectomes(subjRem) = []; 
 coordinates(subjRem) = []; 
 distances(subjRem) = []; 
@@ -53,11 +68,13 @@ for s=1:numSubj
 end
 
 meanDist = mean(dist,3); 
+
 % make a selected version of the group matrix
 if strcmp(groupMatrixType, 'lengthCV')
     G = fcn_group_average(adjMatr,meanDist,hemiid);
 elseif strcmp(groupMatrixType, 'variance')
-    G = giveMeGroupAdj_variance(connectomes, 0.15); 
+    % G = giveMeGroupAdj_variance(connectomes, 0.15); 
+    G = giveMeGroupAdj_variance(connectomes, dens); 
     % thisline can be changed to G = giveMeGroupAdj_variance(connectomes, dens); 
     % dens - will specify
     % the desired density of the connectome; by default the sensity will be
@@ -66,3 +83,6 @@ elseif strcmp(groupMatrixType, 'consistency')
     [G,D] = giveMeGroupAdj_consistency(connectomes, distances, threshold);
 end
 
+% Filter to the right hemisphere cortex
+RH_full = G(hemiid==2, hemiid==2);
+RH_cortex = RH_full(1:180, 1:180);
